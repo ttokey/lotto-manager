4819a9c589696f653dfee537555fbd5e77953b48
ttokey <ttokey.lee@gmail.com>
2021-08-23 23:55:53 +0900

    init

---
+++ src/main/java/com/ttokey/lottomanager/LottoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ttokey.lottomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottoManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ttokey/lottomanager/LottoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttokey.lottomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
